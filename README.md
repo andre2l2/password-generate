9227de49bf61d9977172467155d43525a97e3d3d
Andre Ferreira <delcferreira@gmail.com>
2020-11-07 21:24:33 -0300

    create readme and modify script.js

---
+++ README.md
@@ -0,0 +1,17 @@
+# Generate Password
+
+<p aling="center">
+    <img src="./img/demo.gif">
+<p>
+
+## User stories
+
+[x] User can select the length of the generated password.
+
+[x] User can select one or multiple of the following: Include uppercase letters, Include lowercase letters, Include numbers, Include symbols.
+
+[x] By clicking the Generate password button, the user can see a password being generated.
+
+[x] User can click a Copy to clipboard button which will save the password to the clipboard.
+
+[x] User can see the password strength
--- script.js
+++ script.js
@@ -1,46 +1,56 @@
 // add the event click in button Generate
 document.querySelector('.btn-generate')
     .addEventListener('click', () => {
-        checkAllBox();
+        start();
 })
 
+// create event click for button copy, and copy password in 
 document.querySelector('#generate button')
     .addEventListener('click', () => {
-        const $generate = document.querySelector('#generate input');
-        $generate.select();
+        document.querySelector('#generate input').select();
         document.execCommand('copy');
     })
 
-// this is a caracter list object
-const listCaracter = {
-    uppercase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-    lowercase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
-    numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    symbols: ['-', '!', '@', '#', '$', '%', '&']
-}
-    
-function checkAllBox() {
-    const $lenghtPassword = document.querySelector('#length-password input').value
-    const $allBox = document.querySelectorAll('#checkbox input');
+function start() {
+    // this is a caracter list object
+    const listCaracter = {
+        uppercase: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+        lowercase: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+        numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+        symbols: ['-', '!', '@', '#', '$', '%', '&']
+    }
 
-    generateHash($lenghtPassword, $allBox[0].checked, $allBox[1].checked, $allBox[2].checked, $allBox[3].checked)
-}
+    // function responsible for checking the checkbox
+    function checkAllBox() {
+        const $lenghtPassword = document.querySelector('#length-password input').value;
+        const $allBox = document.querySelectorAll('#checkbox input');
 
-// Generate password function parmas (length, uppercase, lowercase, numbers, symbols)
-function generateHash(
-        lenght = 0, 
-        value1 = false, 
-        value2 = false, 
-        value3 = false, 
-        value4 = false
-    ){ 
-        const { random, floor } = Math;
-        const $generate = document.querySelector('#generate input');
-        let password = '';
+        return [
+            $lenghtPassword,
+            $allBox[0].checked,
+            $allBox[1].checked,
+            $allBox[2].checked,
+            $allBox[3].checked
+        ]
+    }
 
-        if (lenght) {
+    // Generate password function parmas (length, uppercase, lowercase, numbers, symbols)
+    function generateHash(
+            lenght = 0, 
+            value1 = false, 
+            value2 = false, 
+            value3 = false, 
+            value4 = false
+        ){  
+            
+            const { random, floor } = Math;
+
+            const $generate = document.querySelector('#generate input');
+            let password = '';
+
             let c = 0;
-            while (c < lenght) {
+            while (c < lenght && lenght) {
+
                 if (value1 && c < lenght) {
                     c++;
 
@@ -70,7 +80,10 @@ function generateHash(
                     password += listCaracter.symbols[caracter];
                 }
             }
-        }
 
-    $generate.value = password;
+        $generate.value = password;
+    }
+
+    generateHash(...checkAllBox());
 }
+
